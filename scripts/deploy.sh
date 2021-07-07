16ef86d66f65001d6566f0f25e7b4fc6171ce83d
ollyrowe <oliver.rowe101@gmail.com>
2021-07-07 18:39:46 +0100

    Configure custom domain

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,12 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# add CNAME file with custom domain name
+echo 'www.oliverjosephhairdressing.co.uk' > CNAME
+
+# copy index.html to catch 404s
+cp index.html 404.html
+
 git init
 git add -A
 git commit -m 'deploy'
