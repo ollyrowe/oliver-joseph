abee80755f99a00c6882d42cdca6f7b2b7317b08
ollyrowe <oliver.rowe101@gmail.com>
2022-01-10 23:00:48 +0000

    Add moving soon alert

--- src/components/site/body/Body.vue
+++ src/components/site/body/Body.vue
@@ -10,6 +10,20 @@
 
 <script>
 export default {
-  name: "Body"
+  name: "Body",
+  methods: {
+    displayAlert() {
+      this.$buefy.dialog.alert({
+        title: "New for 2022 - We're moving!",
+        message:
+          "As from 18th January 2022, we are moving to a new location in Midsomer Norton...",
+        confirmText: "OK",
+        canCancel: ["outside", "escape"]
+      });
+    }
+  },
+  mounted() {
+    this.displayAlert();
+  }
 };
 </script>
