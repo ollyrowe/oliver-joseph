7a7cdab62c3f0b5898d3b9909b760632507c98ee
ollyrowe <oliver.rowe101@gmail.com>
2022-01-18 22:07:29 +0000

    Configure router scroll behaviour

--- src/router/index.js
+++ src/router/index.js
@@ -43,7 +43,14 @@ export const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  routes
+  routes,
+  scrollBehavior: (_to, _from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 });
 
 export default router;
